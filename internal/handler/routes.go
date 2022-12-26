cdec4b094378d9583180518f5592351c14f4d216
zouchangfu <zcf13543354971@163.com>
2022-12-26 16:45:08 +0800

    refactor: 更改路由路径

--- internal/handler/routes.go
+++ internal/handler/routes.go
@@ -19,37 +19,37 @@ func RegisterHandlers(server *rest.Server, serverCtx *svc.ServiceContext) {
 		[]rest.Route{
 			{
 				Method:  http.MethodGet,
-				Path:    "/user/list",
+				Path:    "/sys/user/list",
 				Handler: user.ListHandler(serverCtx),
 			},
 			{
 				Method:  http.MethodPost,
-				Path:    "/user/add",
+				Path:    "/sys/user/add",
 				Handler: user.AddHandler(serverCtx),
 			},
 			{
 				Method:  http.MethodGet,
-				Path:    "/user/queryById",
+				Path:    "/sys/user/queryById",
 				Handler: user.QueryByIdHandler(serverCtx),
 			},
 			{
 				Method:  http.MethodPut,
-				Path:    "/user/edit",
+				Path:    "/sys/user/edit",
 				Handler: user.EditHandler(serverCtx),
 			},
 			{
 				Method:  http.MethodDelete,
-				Path:    "/user/removeById",
+				Path:    "/sys/user/removeById",
 				Handler: user.RemoveByIdHandler(serverCtx),
 			},
 			{
 				Method:  http.MethodDelete,
-				Path:    "/user/removeByIds",
+				Path:    "/sys/user/removeByIds",
 				Handler: user.RemoveByIdsHandler(serverCtx),
 			},
 			{
 				Method:  http.MethodGet,
-				Path:    "/user/queryLoginUser",
+				Path:    "/sys/user/queryLoginUser",
 				Handler: user.QueryLoginUserHandler(serverCtx),
 			},
 		},
@@ -60,7 +60,7 @@ func RegisterHandlers(server *rest.Server, serverCtx *svc.ServiceContext) {
 		[]rest.Route{
 			{
 				Method:  http.MethodPost,
-				Path:    "/user/login",
+				Path:    "/sys/user/login",
 				Handler: user.LoginHandler(serverCtx),
 			},
 		},
@@ -70,32 +70,32 @@ func RegisterHandlers(server *rest.Server, serverCtx *svc.ServiceContext) {
 		[]rest.Route{
 			{
 				Method:  http.MethodGet,
-				Path:    "/role/list",
+				Path:    "/sys/role/list",
 				Handler: role.ListHandler(serverCtx),
 			},
 			{
 				Method:  http.MethodPost,
-				Path:    "/role/add",
+				Path:    "/sys/role/add",
 				Handler: role.AddHandler(serverCtx),
 			},
 			{
 				Method:  http.MethodGet,
-				Path:    "/role/queryById",
+				Path:    "/sys/role/queryById",
 				Handler: role.QueryByIdHandler(serverCtx),
 			},
 			{
 				Method:  http.MethodPut,
-				Path:    "/role/edit",
+				Path:    "/sys/role/edit",
 				Handler: role.EditHandler(serverCtx),
 			},
 			{
 				Method:  http.MethodDelete,
-				Path:    "/role/removeById",
+				Path:    "/sys/role/removeById",
 				Handler: role.RemoveByIdHandler(serverCtx),
 			},
 			{
 				Method:  http.MethodDelete,
-				Path:    "/role/removeByIds",
+				Path:    "/sys/role/removeByIds",
 				Handler: role.RemoveByIdsHandler(serverCtx),
 			},
 		},
@@ -106,32 +106,32 @@ func RegisterHandlers(server *rest.Server, serverCtx *svc.ServiceContext) {
 		[]rest.Route{
 			{
 				Method:  http.MethodGet,
-				Path:    "/menu/list",
+				Path:    "/sys/menu/list",
 				Handler: menu.ListHandler(serverCtx),
 			},
 			{
 				Method:  http.MethodPost,
-				Path:    "/menu/add",
+				Path:    "/sys/menu/add",
 				Handler: menu.AddHandler(serverCtx),
 			},
 			{
 				Method:  http.MethodGet,
-				Path:    "/menu/queryById",
+				Path:    "/sys/menu/queryById",
 				Handler: menu.QueryByIdHandler(serverCtx),
 			},
 			{
 				Method:  http.MethodPut,
-				Path:    "/menu/edit",
+				Path:    "/sys/menu/edit",
 				Handler: menu.EditHandler(serverCtx),
 			},
 			{
 				Method:  http.MethodDelete,
-				Path:    "/menu/removeById",
+				Path:    "/sys/menu/removeById",
 				Handler: menu.RemoveByIdHandler(serverCtx),
 			},
 			{
 				Method:  http.MethodDelete,
-				Path:    "/menu/removeByIds",
+				Path:    "/sys/menu/removeByIds",
 				Handler: menu.RemoveByIdsHandler(serverCtx),
 			},
 		},
@@ -142,32 +142,32 @@ func RegisterHandlers(server *rest.Server, serverCtx *svc.ServiceContext) {
 		[]rest.Route{
 			{
 				Method:  http.MethodGet,
-				Path:    "/permission/list",
+				Path:    "/sys/permission/list",
 				Handler: permission.ListHandler(serverCtx),
 			},
 			{
 				Method:  http.MethodPost,
-				Path:    "/permission/add",
+				Path:    "/sys/permission/add",
 				Handler: permission.AddHandler(serverCtx),
 			},
 			{
 				Method:  http.MethodGet,
-				Path:    "/permission/queryById",
+				Path:    "/sys/permission/queryById",
 				Handler: permission.QueryByIdHandler(serverCtx),
 			},
 			{
 				Method:  http.MethodPut,
-				Path:    "/permission/edit",
+				Path:    "/sys/permission/edit",
 				Handler: permission.EditHandler(serverCtx),
 			},
 			{
 				Method:  http.MethodDelete,
-				Path:    "/permission/removeById",
+				Path:    "/sys/permission/removeById",
 				Handler: permission.RemoveByIdHandler(serverCtx),
 			},
 			{
 				Method:  http.MethodDelete,
-				Path:    "/permission/removeByIds",
+				Path:    "/sys/permission/removeByIds",
 				Handler: permission.RemoveByIdsHandler(serverCtx),
 			},
 		},
@@ -178,32 +178,32 @@ func RegisterHandlers(server *rest.Server, serverCtx *svc.ServiceContext) {
 		[]rest.Route{
 			{
 				Method:  http.MethodGet,
-				Path:    "/dict/list",
+				Path:    "/sys/dict/list",
 				Handler: dict.ListHandler(serverCtx),
 			},
 			{
 				Method:  http.MethodPost,
-				Path:    "/dict/add",
+				Path:    "/sys/dict/add",
 				Handler: dict.AddHandler(serverCtx),
 			},
 			{
 				Method:  http.MethodGet,
-				Path:    "/dict/queryById",
+				Path:    "/sys/dict/queryById",
 				Handler: dict.QueryByIdHandler(serverCtx),
 			},
 			{
 				Method:  http.MethodPut,
-				Path:    "/dict/edit",
+				Path:    "/sys/dict/edit",
 				Handler: dict.EditHandler(serverCtx),
 			},
 			{
 				Method:  http.MethodDelete,
-				Path:    "/dict/removeById",
+				Path:    "/sys/dict/removeById",
 				Handler: dict.RemoveByIdHandler(serverCtx),
 			},
 			{
 				Method:  http.MethodDelete,
-				Path:    "/dict/removeByIds",
+				Path:    "/sys/dict/removeByIds",
 				Handler: dict.RemoveByIdsHandler(serverCtx),
 			},
 		},
